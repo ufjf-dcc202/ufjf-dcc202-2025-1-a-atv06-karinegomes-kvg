1c65b73702ea60d04d4c27c17185fe25acf05e60
Karine <karine.gomes@estudante.ufjf.br>
2025-08-18 16:58:10 -0300

    tabuleiro inicial

---
+++ main.js
@@ -0,0 +1,13 @@
+// Tabuleiro tradicional em formato de cruz (7x7)
+// 0 = Posição inválida
+// 1 = Posição com peça
+// 2 = Posição vazia
+const tabuleiroInicial = [
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [1, 1, 1, 2, 1, 1, 1],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+];
