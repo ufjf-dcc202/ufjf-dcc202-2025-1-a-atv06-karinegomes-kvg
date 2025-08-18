462d4441669237dc289c7d683d9d3c7ef99acc1d
Karine <karine.gomes@estudante.ufjf.br>
2025-08-18 17:09:04 -0300

    adiciona função mover

--- main.js
+++ main.js
@@ -34,3 +34,42 @@ export function selecionarPeca(linha, coluna) {
         pecaSelecionada = { linha, coluna };
     }
 }
+
+export function moverPeca(linhaDestino, colunaDestino) {
+    if (!pecaSelecionada) return; 
+
+    const { linha: linhaOrigem, coluna: colunaOrigem } = pecaSelecionada;
+
+    
+    if (ehMovimentoValido(linhaOrigem, colunaOrigem, linhaDestino, colunaDestino)) {
+        tabuleiro[linhaDestino][colunaDestino] = 1;
+        tabuleiro[linhaOrigem][colunaOrigem] = 2; 
+
+        
+        const linhaMeio = linhaOrigem + (linhaDestino - linhaOrigem) / 2;
+        const colunaMeio = colunaOrigem + (colunaDestino - colunaOrigem) / 2;
+        tabuleiro[linhaMeio][colunaMeio] = 2;
+
+        pecaSelecionada = null; 
+    }
+}
+
+function ehMovimentoValido(l1, c1, l2, c2) {
+    if (tabuleiro[l2][c2] !== 2) {
+        return false;
+    }
+
+    const ehSaltoHorizontal = Math.abs(c1 - c2) === 2 && l1 === l2;
+    const ehSaltoVertical = Math.abs(l1 - l2) === 2 && c1 === c2;
+
+    if (!ehSaltoHorizontal && !ehSaltoVertical) {
+        return false;
+    }
+
+    const linhaMeio = l1 + (l2 - l1) / 2;
+    const colunaMeio = c1 + (c2 - c1) / 2;
+    if (tabuleiro[linhaMeio][colunaMeio] !== 1) {
+        return false;}
+
+    return true; 
+}
